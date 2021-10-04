53da7ba2f3a42318fb3966db3e5a8814e5b1791f
Эдуард Карпов <karpovfriend1997@gmail.com>
2021-10-04 10:45:55 +0300

    Финальная версия программы. Добавлена кнопка "Сохранить", которая находит и присваивает ID контакта, которому будет отправлено сообщение, записывает данное ID в переменную и отправка сообщений теперь может происходить большее количество раз до срабатывания таймаута на отправление,  раньше можно было отправить 5 сообщений до страбатывания метода телеграмм API timetowait() , теперь при небыстром нажатии на кнопки для отправки сообщений , количество сообщений на отправку не ограничевается методом. Поправлена логика работы всех кнопок, отвечающих за отправку сообщений, теперь они используют только 2 метода : await client.ConnectAsync();                 await client.SendMessageAsync(new TLInputPeerUser() { UserId = ID }, button.Text) Изменена визуальная состовляющая программы, подобраны цвета фона и кнопок по просьбе заказчика. Добавлена иконка приложения.

--- Send_telegram/Form1.cs
+++ Send_telegram/Form1.cs
@@ -21,77 +21,68 @@ namespace Send_telegram
         private readonly static string apiHash = "ee92079c288690b01e5c22d018789808";
         static string phoneNumber = "";
         TelegramClient client = new TelegramClient(apiId, apiHash);
+        int ID = 0;
         public Form1()
         {
             InitializeComponent();
             AutoCompleteStringCollection source_1 = new AutoCompleteStringCollection()
         {
-            "+79200920572",
-            "+79605162371",
+            "+79605162371"
         };
             textBox2.AutoCompleteCustomSource = source_1;
             textBox2.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             textBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
             AutoCompleteStringCollection source_2 = new AutoCompleteStringCollection()
-        {
-            "Даня"        
-        };
+            {
+                "Данила",
+            };
             textBox3.AutoCompleteCustomSource = source_2;
             textBox3.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             textBox3.AutoCompleteSource = AutoCompleteSource.CustomSource;
-            Button[] Mass_Button = new Button[55] { button1, button3, button5, button7, button9, button10, button11, button12,
-            button13, button14, button15, button16, button17, button18, button19, button21,
+            Button[] Mass_Button = new Button[61] { button1, button2, button3, button4, button5,button6, button7, button8, button9, button10, button11, button12,
+            button13, button14, button15, button16, button17, button18, button19,button20, button21,button22,
             button23, button25, button27, button28, button29, button30, button31, button32,
             button34, button36, button38, button40, button41, button42, button43, button44,
             button45, button47, button49, button51, button53, button54, button55, button56,
             button57, button58, button60, button62, button64, button66, button67, button68,
              button69, button70, button71, button73, button75, button77, button79};
-           
-                foreach (Button button in Mass_Button)
+
+            foreach (Button button in Mass_Button)
+            {
+                CheckForIllegalCrossThreadCalls = false; // нехороший лайфхак,
+                                                         // отменяет отслеживание ошибок,
+                                                         // но дает передать компоненты формы в другой поток 
+                button.Click += async (s, e) =>
                 {
-                    CheckForIllegalCrossThreadCalls = false; // нехороший лайфхак,
-                                                             // отменяет отслеживание ошибок,
-                                                             // но дает передать компоненты формы в другой поток 
-                    button.Click += async (s, e) =>
+                    for (int i = 0; i < Mass_Button.Length; i++)
                     {
-                        for (int i = 0; i < Mass_Button.Length; i++)
+                        if (button == Mass_Button[i])
                         {
-                            if (button == Mass_Button[i])
-                            {
-                                await Task.Run(() => Message(button));
-                            }
+                            await Task.Run(() => Message(button));
                         }
-                    };
-                }
-            
+                    }
+                };
+            }
         }
         async public void Message(Button button)
         {
-            if (textBox3.Text != "")
+
+            try
             {
-                try
-                {
-                    await client.ConnectAsync();
-                    var result = await client.GetContactsAsync();
-                    var user = result.Users
-                        .Where(x => x.GetType() == typeof(TLUser))
-                        .Cast<TLUser>()
-                        .FirstOrDefault(x => x.FirstName == textBox3.Text);
-                    await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, button.Text);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Ошибка: \n{ex}");
-                }
+                await client.ConnectAsync();
+                await client.SendMessageAsync(new TLInputPeerUser() { UserId = ID }, button.Text);
             }
-            else
-                MessageBox.Show($"Поле для ввода имени получателя сообщения - пустое");
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: \n{ex}");
+            }
+
         }
         async private void button13_Click(object sender, EventArgs e)
         {
-            await Task.Run(()=> AuthenticationAsync());
+            await Task.Run(() => AuthenticationAsync());
         }
-      async public void AuthenticationAsync()
+        async public void AuthenticationAsync()
         {
             phoneNumber = textBox2.Text;
             await client.ConnectAsync();
@@ -99,5 +90,30 @@ namespace Send_telegram
             var code = textBox1.Text;
             var user = await client.MakeAuthAsync(phoneNumber, hash, code);
         }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            SendAsync();
+        }
+        async public void SendAsync()
+        {
+            if (textBox3.Text != "")
+            {
+                await client.ConnectAsync();
+                var result = await client.GetContactsAsync();
+                var user = result.Users
+                    .Where(x => x.GetType() == typeof(TLUser))
+                    .Cast<TLUser>()
+                    .FirstOrDefault(x => x.FirstName == textBox3.Text);
+                ID = user.Id;
+            }
+            else
+                MessageBox.Show($"Поле для ввода имени получателя сообщения - пустое");
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    SendAsync();
+        //}
     }
 }
